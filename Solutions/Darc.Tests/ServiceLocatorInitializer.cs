1303efe4473d9f83ddc83e7f68cbe8442b3c7612
Meryl <aa5151521@126.com>
2016-11-02 22:13:17 +0800

    modify some bugs

--- Solutions/Darc.Tests/ServiceLocatorInitializer.cs
+++ Solutions/Darc.Tests/ServiceLocatorInitializer.cs
@@ -1,65 +1,24 @@
 ﻿namespace Darc.Tests
 {
-    using Castle.DynamicProxy;
     using Castle.MicroKernel.Registration;
-    using Castle.Windsor;
-    using CommonServiceLocator.WindsorAdapter;
     using Core;
-    using Core.Contracts;
-    using Core.Interceptor;
+    using Core.Castle;
     using Dapper;
-    using Dapper.Common;
-    using Microsoft.Practices.ServiceLocation;
 
     public class ServiceLocatorInitializer
     {
         public static void Init()
         {
-            IWindsorContainer container = new WindsorContainer();
-
-            AddCommentsTo(container);
-            AddGenericRepositoriesTo(container);
-            AddQueryObjectsTo(container);
-            AddTasksTo(container);
-
-            ServiceLocator.SetLocatorProvider(() => new WindsorServiceLocator(container));
-        }
-
-        private static void AddCommentsTo(IWindsorContainer container)
-        {
-            container.Register(
-                Component.For(typeof (IDataContext))
-                    .ImplementedBy(typeof (DataContext))
-                    .Named("DataSession"));
-
-            container.Register(
-                Component.For<IInterceptor>()
-                    .ImplementedBy<TransactionInterceptor>()
-                    .Named("Transaction"));
-        }
-
-        private static void AddGenericRepositoriesTo(IWindsorContainer container)
-        {
-            container.Register(
-                Component.For(typeof (ICommandProcessor))
-                    .ImplementedBy(typeof (CommandProcessor))
-                    .Named("commandProcessor"));
-        }
-
-        private static void AddQueryObjectsTo(IWindsorContainer container)
-        {
-            container.Register(
-                AllTypes.FromAssemblyNamed("Darc.Queries")
-                    .BasedOn(typeof (DapperQuery))
-                    .WithService.DefaultInterfaces());
-        }
-
-        private static void AddTasksTo(IWindsorContainer container)
-        {
-            container.Register(
-                AllTypes.FromAssemblyNamed("Darc.Commands")
+            CastleRegistrer.Initialize().AddComponentsTo(p =>
+            {
+                p.Register(AllTypes.FromAssemblyNamed("Darc.Commands")
                     .BasedOn(typeof (CommandBase))
                     .WithService.FirstInterface());
+
+                p.Register(AllTypes.FromAssemblyNamed("Darc.Queries")
+                    .BasedOn(typeof (DapperQuery))
+                    .WithService.DefaultInterfaces());
+            });
         }
     }
 }
